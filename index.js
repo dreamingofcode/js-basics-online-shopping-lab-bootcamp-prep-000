3e702f30d32e18c68a8c809529316594e70b6f54
cesar mota <cesarmota@ymail.com>
2019-03-13 03:36:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,13 +10,39 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  let price= Math.floor(Math.random() * 100);
+  var itemAdded={itemName:item, itemPrice:price};
+  cart.push(itemAdded);
+  return `${itemAdded.itemName}`+" has been added to your cart."
+  
 }
+
 
 function viewCart() {
-  // write your code here
+  //var cartitems=[];  
+  
+  if (cart.length===0){
+    return (`Your shopping cart is empty.`)
+  }
+ 
+  else if(cart.length===1){
+      return (`In your cart, you have ${getCart()[0].itemName} at $${getCart()[0]//.itemPrice}`) 
+      }
+    
+    //${cart[0].itemName} at $${cart[0].itemPrice}.`)
+    
+  }
+    
+    
+    //for (var i=0; i<cart.length;i++){
+     // cartitems.push(`${getCart()[i].itemName} at $${getCart()[i]//.itemPrice}`) 
+ //   }
+   // }
+ // return `In your cart, you have ${cartitems.join(", and ")}`
+  //else if (cart.length>1){
+    //return `In your cart, you have ${cartitems.join(", and ")}`
+  //}
 }
-
 function total() {
   // write your code here
 }
